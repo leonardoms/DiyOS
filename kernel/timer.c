0c756ca143b950d07fd4fb288327c4a83f02554a
leonardoms <leonardoms1911@gmail.com>
2018-01-17 02:43:35 -0200

    .

--- kernel/timer.c
+++ kernel/timer.c
@@ -19,8 +19,8 @@ IRQN(0,timer_handle); // create irq0 function
 
 void setup_timer(){
   unsigned short f = 1193181 / 100; // 100 = 100hz -> 0.01s
-	outportb(0x43, 0x36);
-	outportb(0x40, f & 0xFF);
-	outportb(0x40, f >> 8);
-	irq_install(0,irq0);
+  outportb(0x43, 0x36);
+  outportb(0x40, f & 0xFF);
+  outportb(0x40, f >> 8);
+  irq_install(0,irq0);
 }
