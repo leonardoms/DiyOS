1277d02e707e672837c028b123db8050f5e11123
leonardoms <leonardoms1911@gpixel.org>
2020-01-06 11:59:20 -0300

    lib/ring.c

--- include/ring.h
+++ include/ring.h
@@ -4,9 +4,28 @@
 #include <stdlib.h>
 
 typedef struct ring {
+  uint32_t  magic;
   uint8_t*  buffer;
-  uint32_t  head, tail;
-  uint32_t  lenght;
+  uint32_t  read, write;
+  uint32_t  lenght, count;
 } ring_t;
+
+uint8_t
+ring_init(ring_t* r, uint32_t size);
+
+uint8_t
+ring_is_full(ring_t* r);
+
+uint8_t
+ring_is_empty(ring_t* r);
+
+uint32_t
+ring_size(ring_t* r);
+
+uint8_t
+ring_push(ring_t* r, uint8_t data);
+
+uint8_t
+ring_pop(ring_t* r, uint8_t* data);
 
 #endif
